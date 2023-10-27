881160851990ed238b905820eb6fff221ed5126b
Shayon S. Kundu <90950526+ShayonKundu@users.noreply.github.com>
2023-10-27 17:26:29 +0530

    Add files via upload

---
+++ Cattle Connect/script.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 5,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.querySelector("#login-btn").onclick = () => {
+  document.querySelector(".login-form-container").classList.toggle("active");
+};
+
+document.querySelector("#close-login-form").onclick = () => {
+  document.querySelector(".login-form-container").classList.remove("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
